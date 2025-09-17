a0b4a34016ef89656fbbdcb2804285e7443226f4
AndreySamaev <andreysamaev@gmail.com>
2025-09-17 15:55:01 +0300

    start structure

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
 
 <template>
   <div class="container">
-   <div>
+   <div class="panel">
     <div class="header">
      <h1>User Management Dashboard</h1>
+    </div>
+    <div class="footer">
+     <span>Demo with mock data & delays</span>
+     <span>Vue 3 • Composition API • TypeScript</span>
     </div>
    </div>
 
