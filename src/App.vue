13de25987adda0f95b761c5021594bc7a3a3a339
AndreySamaev <andreysamaev@gmail.com>
2025-09-17 15:52:25 +0300

    root styles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container">
+   <div>
+    <div class="header">
+     <h1>User Management Dashboard</h1>
+    </div>
+   </div>
+
+  </div>
 </template>
 
 <style scoped></style>
